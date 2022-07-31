a3a4e8486605e5c8b03201c87a1628c741280dfc
Sam Ellis <samwilliamellis@gmail.com>
2022-07-31 09:49:48 +0100

    chore: remove typo

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,4 @@
--- Options to go add `gf` functionality inside `.lua` files.
+-- Options to add `gf` functionality inside `.lua` files.
 vim.opt_local.path:prepend(vim.fn.stdpath('config'))
 vim.opt_local.include = [=[\v<((do|load)file|require)\s*\(?['"]\zs[^'"]+\ze['"]]=]
 vim.opt_local.includeexpr = 'v:lua.Lua_GF_Search(v:fname)'
